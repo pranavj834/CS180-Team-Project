a9ed62d68a943446182f0ae042783e47b611df10
pranavj834 <jastip@purdue.edu>
2025-11-06 14:37:37 -0500

    Create Reservation2.java

---
+++ Reservation2.java
@@ -0,0 +1,14 @@
+public class Reservation {
+	private String date;
+	private String time;
+    private int amtOfPeople;
+    
+	public Reservation(String date, String time, int amtOfPeople) {
+		this.date = date;
+		this.time = time;
+        this.amtOfPeople = amtOfPeople;
+	}
+    
+    
+	
+}
