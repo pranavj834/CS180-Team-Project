40cc4ab3d9847c76eea368bb97b79af03ed126c7
pranavj834 <jastip@purdue.edu>
2025-11-08 16:04:51 -0500

    Update UserAccInterface.java with documentation

--- UserAccInterface.java
+++ UserAccInterface.java
@@ -2,13 +2,6 @@ import java.util.ArrayList;
 
 public interface UserAccInterface {
     
-    //concrete methods
-    boolean addReservation(Reservation res); //method that adds a reservation
-    boolean removeReservation(Reservation res); //method that cancels a reservation
-    boolean equals(String enteredUserName, String enteredPassword); //method to check if the entered username and password are correct.
-    void deleteAccount(); //method to delete account
-    String toString(); //method to print account details (added in case just to be safe)
-
     //getter methods
     String getUsername(); //returns username
     String getFullName(); //returns full name
@@ -20,4 +13,11 @@ public interface UserAccInterface {
     void setFullName(String fullName); //sets full name
     void setEmail(String email); //sets email
     void setPassword(String password); //sets password
+
+    //other methods
+    boolean addReservation(Reservation res); //method that adds a reservation
+    boolean removeReservation(Reservation res); //method that cancels a reservation
+    boolean equals(String enteredUserName, String enteredPassword); //method to check if the entered username and password are correct.
+    void deleteAccount(); //method to delete account
+    String toString(); //method to print account details (added in case just to be safe)
 }
