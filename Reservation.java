9b5bc69b219544600a151fb7dbe2269c106e6776
pranavj834 <jastip@purdue.edu>
2025-11-08 16:51:53 -0500

    Update Reservation.java with documentation

--- Reservation.java
+++ Reservation.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
 public class Reservation implements ResInterface {
+
+	//instances variables; variables are based on whatever is important to create reservations
 	private String date;
 	private String time;
     private int numPeople;
 	private ArrayList<Integer> seats;
-    
+
+	//constructor; sets the respective instance variables based on their respective inputs given
 	public Reservation(String date, String time, int numPeople, ArrayList<Integer> seats) {
 		this.date = date;
 		this.time = time;
@@ -13,34 +16,30 @@ public class Reservation implements ResInterface {
 		this.seats = seats;
 	}
 
+	//getters; returns the respective instance variables based on their respective inputs given
 	public String getDate() {
 		return date;
 	}
-
 	public String getTime() {
 		return time;
 	}
-
 	public int getNumPeople() {
 		return numPeople;
 	}
-
 	public ArrayList<Integer> getSeats() {
 		return seats;
 	}
 
+	//setters; sets the respective instance variables based on their respective inputs given
 	public void setDate(String date) {
 		this.date = date;
 	}
-
 	public void setTime(String time) {
 		this.time = time;
 	}
-
 	public void setNumPeople(int numPeople) {
 		this.numPeople = numPeople;
 	}
-
 	public void setSeats(ArrayList<Integer> seats) {
 		this.seats = seats;
 	}
