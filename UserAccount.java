70a2ab454fe1cff4564860031c5a25ae7ebf8eea
pranavj834 <jastip@purdue.edu>
2025-11-08 18:20:57 -0500

    Update UserAccount.java with documentation

--- UserAccount.java
+++ UserAccount.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
-
+/**
+ * A class that creates accounts for users who want to create reservations at a restaurant.
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2025</p>
+ *
+ * @author Pranav Jasti, lab sec L23
+ * @version November 8, 2025
+ */
 public class UserAccount implements UserAccInterface {
 	
 	//instance variables (usual attributes of an account)
