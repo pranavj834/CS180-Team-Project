4dded34b46f4e239144fe7183ebc26a52ffd8e7e
pranavj834 <jastip@purdue.edu>
2025-11-08 16:28:07 -0500

    Update ResInterface.java with documentation

--- ResInterface.java
+++ ResInterface.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 
 public interface ResInterface {
-    String getDate();
-    String getTime();
-    int getNumPeople();
-    ArrayList<Integer> getSeats();
+    //getters
+    String getDate(); //returns the date for the reservation
+    String getTime(); //returns the time for the reservation
+    int getNumPeople(); //returns the number of people reserving the seats
+    ArrayList<Integer> getSeats(); //returns the seats of the people who booked the reservation
 
-    void setDate(String date);
-    void setTime(String time);
-    void setNumPeople(int numPeople);
-    void setSeats(ArrayList<Integer> seats);
+    void setDate(String date); //sets the date for the reservation
+    void setTime(String time); //sets the time for the reservation
+    void setNumPeople(int numPeople); //sets the number of people reserving the seats
+    void setSeats(ArrayList<Integer> seats); //sets aka reserves the seats of the people who booked the reservation
 }
