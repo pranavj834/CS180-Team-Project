9f9e4e5095525b5f88fd53b56f43a251df85f1fc
pranavj834 <jastip@purdue.edu>
2025-11-08 16:02:04 -0500

    Update UserAccount.java

--- UserAccount.java
+++ UserAccount.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 
 public class UserAccount implements UserAccInterface {
+	
+	//instance variables (usual attributes of an account)
 	private String username;
 	private String fullName;
 	private String email;
 	private String password;
-	private ArrayList<Reservation> reservations;
+	private ArrayList<Reservation> reservations; //tracks all the user's reservations
 
+	//constructor; sets all the details of the account like username and password to the given inputs in the parameters
 	public UserAccount(String username, String password, String fullName, String email) {
         this.username = username;
         this.password = password;
@@ -15,6 +18,7 @@ public class UserAccount implements UserAccInterface {
 		reservations = new ArrayList<>();
     }
 
+	//sets all the instance variables to null to delete the account 
 	public void deleteAccount() {
 		username = null;
 		password = null;
@@ -23,6 +27,7 @@ public class UserAccount implements UserAccInterface {
 		reservations = null;
 	}
 
+	//checks to see if the entered username and password is correct
 	public boolean equals(String enteredUserName, String enteredPassword) {
 		if (enteredUserName.equals(username) && enteredPassword.equals(password)) {
 			return true;
@@ -30,49 +35,47 @@ public class UserAccount implements UserAccInterface {
 		return false;
 	}
 
+	//books a reservation
 	public boolean addReservation(Reservation res) {
 		return reservations.add(res);
 	}
 
+	//cancels a reservation
 	public boolean removeReservation(Reservation res) {
 		return reservations.remove(res);
 	}
-
+	//prints the account details in case necessary
 	public String toString() {
 		return "UserAccount:\n" +
 				"username = " + username + "\n" +
 				"fullName = " + fullName + "\n" +
 				"email = " + email;
 	}
-	
+
+	//getter methods; gives the respective instance variable to the respective inputs given
 	public String getUsername() {
 		return username; 
 	}
-
-    public String getFullName() { 
+    public String getFullName() {
     	return fullName; 
     }
-
-    public String getEmail() { 
+    public String getEmail() {
     	return email;
     }
-
 	public ArrayList<Reservation> getReservations() {
 		return reservations;
 	}
 
+	//setter methods; sets the respective instance variable to the respective inputs given
 	public void setUsername(String username) {
     	this.username = username;
     }
-
     public void setFullName(String fullName) { 
     	this.fullName = fullName;
     }
-
     public void setEmail(String email) { 
     	this.email = email; 
     }
-
     public void setPassword(String password) {
     	this.password = password;
     }
